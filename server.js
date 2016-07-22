2f6ea302d4b48c2e5c787a33aa1c2bb03aa88123
woniesong92 <hs459@cornell.edu>
2016-07-21 21:22:02 -0700

    Add CORS

--- server.js
+++ server.js
@@ -3,16 +3,21 @@ import { apolloServer } from 'apollo-server';
 import Schema from './data/schema';
 import Mocks from './data/mocks';
 import Resolvers from './data/resolvers';
+import cors from 'cors';
 
 const GRAPHQL_PORT = 8080;
 
 const graphQLServer = express();
+
+graphQLServer.use(cors());
+
 graphQLServer.use('/graphql', apolloServer({
   graphiql: true,
   pretty: true,
   schema: Schema,
   resolvers: Resolvers,
 }));
+
 graphQLServer.listen(GRAPHQL_PORT, () => console.log(
   `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}/graphql`
 ));
